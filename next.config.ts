a39a6c1af5ae34375634e1fa204fd2b10966724f
LeVoNhatPin <pinle2409@gmail.com>
2026-05-13 09:28:49 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/pinlee-portfolio",
+  assetPrefix: "/pinlee-portfolio/",
 };
 
 export default nextConfig;
